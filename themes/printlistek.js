f0ea72d2322971a2a58d60ca645b09420833d16c
admin <igcms@commit.com>
2019-04-12 15:13:43 +0000

    Admin

--- themes/printlistek.js
+++ themes/printlistek.js
@@ -175,7 +175,7 @@
   button.appendChild(span);
   span.innerHTML = 'Vytisknout denní nabídku';
   span.title = button.innerHTML;
-  span.className = 'noprint fa fa-print';
+  span.className = 'noprint fab fa-print';
   span.style = "width: 1em; margin-left: 0.5em; white-space: nowrap; overflow: hidden; border: 0; background: none; cursor: pointer;";
   var nadpis = document.getElementById('denni_nabidka');
   if (!nadpis) {
