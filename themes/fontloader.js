80acc29e323a8c371839eb19c013e493f463e384
admin <igcms@commit.com>
2019-04-12 18:26:16 +0000

    Admin

--- themes/fontloader.js
+++ themes/fontloader.js
@@ -1,8 +1,7 @@
 require("WebFont", function() {
   WebFont.load({
     google: {
-      families: ['Roboto:400,400italic,500,700:latin-ext', 'Cormorant Spectral:200,200italic,300,300italic,500,500italic:latin-ext'],
-//       families: ['Roboto:400,400italic,500,700:latin-ext', 'Cormorant Garamond:400,400italic,600,700,700italic:latin-ext'],
+      families: ['Roboto:400,400italic,500,700:latin-ext', 'EB Garamond:400,400italic,600,700,700italic:latin-ext'],
 //     families: ['Roboto'],
 //     urls: ['/lib/roboto/roboto.css'],
     },
