039ed5dd3fdf26f0a0429205c545490f68a77ad5
admin <igcms@commit.com>
2019-04-12 18:15:22 +0000

    Admin

--- themes/fontloader.js
+++ themes/fontloader.js
@@ -1,7 +1,7 @@
 require("WebFont", function() {
   WebFont.load({
     google: {
-      families: ['Roboto:400,400italic,500,700:latin-ext', 'Merriweather:400,400italic,600,700,700italic:latin-ext'],
+      families: ['Roboto:400,400italic,500,700:latin-ext', 'Merriweather:300,300italic,400,400italic,600,700,700italic:latin-ext'],
 //     families: ['Roboto'],
 //     urls: ['/lib/roboto/roboto.css'],
     },
