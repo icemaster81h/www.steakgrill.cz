155afcb8a2a16091a8c8cfcdf5b9c6849b914ddd
pavel <igcms@commit.com>
2019-11-13 16:41:23 +0000

    Admin

--- themes/fontloader.js
+++ themes/fontloader.js
@@ -2,7 +2,7 @@ require("WebFont", function() {
   WebFont.load({
     google: {
 //       families: ['Roboto:400,400italic,500,700:latin-ext', 'Frank Ruhl Libre:300,400,500,700:latin-ext'],
-      families: ['Roboto:400,400italic,500,700:latin-ext&display=swap', 'EB Garamond:400,400italic,600,700,700italic:latin-ext&display=swap'],
+      families: ['Roboto:400,400italic,500,700:latin-ext', 'EB Garamond:400,400italic,600,700,700italic:latin-ext&display=swap'],
 //     families: ['Roboto'],
 //     urls: ['/lib/roboto/roboto.css'],
     },
