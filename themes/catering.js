930e63cd90cb2fb8702dacae87e05e89289bdda0
pavel <igcms@commit.com>
2019-11-27 18:18:59 +0000

    Admin

---
+++ themes/catering.js
@@ -0,0 +1,26 @@
+(function () {
+  function validateCnt (event, cntInput) {
+    if (event.target.value === "-") {
+      cntInput.setAttribute("min", 0)
+      return
+    }
+    cntInput.setAttribute("min", 1)
+  }
+
+  var starter = document.getElementById("contactform-catering-starter")
+  var starterCnt = document.getElementById("contactform-catering-starter-cnt")
+  starter.addEventListener("change", function (event) {
+    var localEvent = event
+    var localCnt = starterCnt
+    return validateCnt(localEvent, localCnt)
+  }, false)
+
+  var drink = document.getElementById("contactform-catering-drink")
+  var drinkCnt = document.getElementById("contactform-catering-drink-cnt")
+  drink.addEventListener("change", function (event) {
+    var localEvent = event
+    var localCnt = drinkCnt
+    return validateCnt(localEvent, localCnt)
+  }, false)
+
+})()
